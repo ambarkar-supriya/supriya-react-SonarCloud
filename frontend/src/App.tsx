146f7354d9458d5a1e4e485af7510cd28b78bb33
ambarkar-supriya <supriya.ambarkar@zemosolabs.com>
2024-10-17 13:50:02 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Sonar Cloud
         </a>
       </header>
     </div>
